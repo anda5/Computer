15ca891f183aac2ae826812de62a377165760f2d
Anda Lixandroiu <lix.anda@yahoo.com>
2015-11-19 20:40:52 +0200

    Create methods in Convert class

--- src/Convert.java
+++ src/Convert.java
@@ -1,14 +1,36 @@
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.StringTokenizer;
+
 /**
  * Created by anda on 11/19/2015.
  */
 public class Convert {
 
-    private String romanNumber;
+
+    public static int getNumber(String romanNumber){
+        char[] arrayNumbers;
+        int sum;
+
+        if (isValid(romanNumber)) {
+            arrayNumbers = romanNumber.toCharArray();
+            if(arrayNumbers.length%2!=0){
+                romanNumber.toString().concat("");
+            }
+
+          sum = calculateSum(arrayNumbers);
+
+        }
+        else {
+           System.out.println("This is not a valid roman number");
+            sum =0;
+        }
+        return  sum;
+
+    }
 
 
-
-
-     private int getValueOfSimbol(char simbol) {
+     private static int getValueOfSimbol(char simbol) {
         int integerValue = 0;
         switch (simbol) {
             case ('I'):
@@ -37,13 +59,29 @@ public class Convert {
 
     }
 
-    private boolean isValid(String romanNumber) {
+    private static boolean isValid(String romanNumber) {
 
         if (romanNumber.matches("^(M{0,3})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$"))
         { return true;
         }else {
             return false;
         }
+    }
+
+    private static int calculateSum(char[] array){
+        int sum = 0;
+        for(int i=0;i<array.length;i+=2){
+
+            int first =   getValueOfSimbol(array[i]);
+            int second =  getValueOfSimbol(array[i+1]);
+            if(first>=second){
+                sum += first+second;
+            }else if(first<second){
+                sum += second-first;
+            }
+
+        }
+        return sum;
     }
 
 }
